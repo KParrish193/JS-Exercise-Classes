a237a70b1ab3522caa059fabd9824eb13d0f0da6
Kristi Parrish <kp@Kristis-MacBook-Pro.local>
2019-11-14 13:51:12 -0700

    person tests all passing

--- index.js
+++ index.js
@@ -41,6 +41,23 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+    eat (someFood) {
+      if (this.stomach.length < 10)
+      return this.stomach.push(someFood);
+    };
+
+    poop (){
+      return this.stomach.length = 0;
+    };
+
+    toString () {
+      return `${this.name}, ${this.age}`
+    }
 
 }
 
@@ -59,7 +76,10 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+  }
 }
 
 /*
